e6bdc05c8ed3a92be1974d0329eba5994c2ae996
zprovisionz <zprovisionz@gmail.com>
2026-02-21 07:46:44 -0500

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'react-native-reanimated/plugin',
-        {
-          relativeSourceLocation: true,
-        },
-      ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
